ee5473832c56362f157969ff8ffa351a8599ea7d
Ryo Toriumi <torinikov@softbank.ne.jp>
2016-02-21 16:54:11 +0900

    Views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,49 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all
+    @author = Author.find(1)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+   @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+
   end
 
   def edit
+    @post = Post.new(post_params)
   end
 
   def update
   end
 
   def destroy
+    @post = Post.new
   end
+
+private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category)
+  end
+
 end
